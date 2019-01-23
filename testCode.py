ac2a965573d220d2b4b0a4303cc32b45b2a7d03a
tjayleezy <tyler.leary@westpoint.edu>
2019-01-23 11:00:26 -0500

    added the median

--- testCode.py
+++ testCode.py
@@ -2,4 +2,9 @@ def avg(name):
 	return sum(name)/len(name)
 	
 	
-print(avg([1,2,3,7,5]))
+print(avg([1,2,3,7,5,6]))
+
+def median(nums):
+    return nums[len(nums)//2]
+
+print(median([1,2,3,4,5,6]))
